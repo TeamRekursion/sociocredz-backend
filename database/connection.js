06de68ee66191009c9a438a0620c0969e644db4a
Md Hishaam Akhtar <hishaamakhtar2001.mha@gmail.com>
2021-03-20 14:35:46 +0530

    fix: enable ssl for heroku

--- database/connection.js
+++ database/connection.js
@@ -5,13 +5,13 @@ dotEnv.config()
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false
-  //   }
-  // }
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 })
 
 module.exports = sequelize
