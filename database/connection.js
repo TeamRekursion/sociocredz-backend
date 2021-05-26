db499a8b56b63ce44d224e7d7380c3ac145ca1d7
Mayank Kumar <mayank22oct@gmail.com>
2021-05-26 20:36:28 +0530

    refactor: db env var for refault database

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const dotEnv = require('dotenv')
 
 dotEnv.config()
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DB || process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
   dialectOptions: {
